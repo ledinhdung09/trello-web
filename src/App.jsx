3ae54f6a0b84b639da2d6bf8fcf7a5baa34b897d
dung001 <ledinhdung.net@gmail.com>
2024-10-29 18:53:13 +0700

    refactor project structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Button from "@mui/material/Button";
+import Board from "./pages/Boards/_id";
 
 function App() {
   return (
     <>
-      <div>ledinhdung</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Board />
     </>
   );
 }
